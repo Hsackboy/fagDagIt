b80dc7f14c21879f81420264e03e786e527585f7
emilopdal <emil.opdal.2006@gmail.com>
2024-02-20 12:20:02 +0100

    La til funksjonen for å redigere fil

--- tekst editor code.py
+++ tekst editor code.py
@@ -1,12 +1,12 @@
-filbane = "testen_min.txt"
+def tekst_editor(filbane):
+    tekst = input(str("Skriv teksten du ønsker og legge til:"))
+    ny_linje = input(str("Vil du begynne på en ny linje?"))
+    if ny_linje =="ja":
+        tekst = "\n" + tekst
+    with open (filbane, "a", encoding="utf-8") as fil:
+        fil.write(tekst)
 
-tekst = input(str("Skriv teksten du ønsker og legge til:"))
-
-ny_linje = input(str("Vil du begynne på en ny linje?"))
-if ny_linje =="ja":
-    tekst = "\n" + tekst
-
-with open (filbane, "a", encoding="utf-8") as fil:
-    fil.write(tekst)
+if __name__ == "__main__":
+    tekst_editor ("test.txt")
 
 
