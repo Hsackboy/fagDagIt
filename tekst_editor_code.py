084307a94bfc1c95cb7f1247cc3afbf230687148
emilopdal <emil.opdal.2006@gmail.com>
2024-02-20 13:16:46 +0100

    laget en funksjon som teller ord

--- tekst_editor_code.py
+++ tekst_editor_code.py
@@ -1,3 +1,4 @@
+import kultBiblotek2 as kB
 def tekst_editor(filbane):
     tekst = input(str("Skriv teksten du ønsker og legge til:"))
     ny_linje = input(str("Vil du begynne på en ny linje?"))
@@ -5,8 +6,20 @@ def tekst_editor(filbane):
         tekst = "\n" + tekst
     with open (filbane, "a", encoding="utf-8") as fil:
         fil.write(tekst)
+        fil.flush()
+        fil.close()
+
+
+def tell_ord(filbane):
+    antall = 0
+    with open(filbane,"r",encoding="utf-8") as fil:
+        
+        for linje in fil:
+            ord_i_linje = linje.split()
+            antall += len(ord_i_linje)
+    print("Antall ord:", antall)
+
 
 if __name__ == "__main__":
     tekst_editor ("test.txt")
-
-
+    tell_ord ("test.txt")
