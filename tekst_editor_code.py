23f30da7b4da9115452d5e03733d19c8ef89990e
=Håkon Mikalsen <=hakmikalsen@gmail.com>
2024-02-20 12:55:48 +0100

    la til filleserbiblotek, redigerte tekseditor funksjonen, la til samkjøring i main.py og lagde funksjon for å opprette fil

--- tekst_editor_code.py
+++ tekst_editor_code.py
@@ -1,12 +1,26 @@
+import kultBiblotek2 as kB
+import filLeserBiblotek2 as fB
+
 def tekst_editor(filbane):
     tekst = input(str("Skriv teksten du ønsker og legge til:"))
-    ny_linje = input(str("Vil du begynne på en ny linje?"))
+    ny_linje = kB.betterInput("str","Vil du ha ny linje? ","",forventet=["ja","nei","Ja","Nei"])
     if ny_linje =="ja":
         tekst = "\n" + tekst
     with open (filbane, "a", encoding="utf-8") as fil:
         fil.write(tekst)
 
+
+
+def opprettFil(filbane):
+    filbane +="/"+kB.betterInput("str","Hva skal den nye filen hete? ","Dette var ikke rett")+".txt"
+    tekst =kB.betterInput("str","Hva skal stå i filen? ","Dette var ikke rett")
+    fB.skrivTilNyFil(filbane,tekst,safe=False)
+        
+
+
+
 if __name__ == "__main__":
     tekst_editor ("test.txt")
+    
 
 
