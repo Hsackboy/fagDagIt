2a96520c71de829597b85984af7780318b694470
=Håkon Mikalsen <=hakmikalsen@gmail.com>
2024-02-20 10:36:56 +0100

    Gjorde finn fil til en dunksjon og la til noen testmapper

--- filFinner.py
+++ filFinner.py
@@ -1,23 +1,32 @@
 import os 
 import kultBiblotek2 as kB
 
-current_dir = os.getcwd()
-dir_list = os.listdir(current_dir)
-
-print("")
-print("-"*20)
-print("Her er en liste over filer:")
-kB.print1DList(dir_list)
-
-brukerInput = kB.betterInput(variableType="int",inputText=f"Hvilken fil vil du ha?(0-{len(dir_list)-1}): ",errorText="Dette var ikke et heltall",forventet=[i for i in range(len(dir_list))])
-current_dir +="/"+dir_list[brukerInput]
+# current_dir = os.getcwd()
 
 
-if os.path.isdir(current_dir)==True:
+def finnFil(current_dir):
+    dir_list = os.listdir(current_dir)
+
     print("")
     print("-"*20)
-    dir_list = os.listdir(current_dir)
     print("Her er en liste over filer:")
     kB.print1DList(dir_list)
+
     brukerInput = kB.betterInput(variableType="int",inputText=f"Hvilken fil vil du ha?(0-{len(dir_list)-1}): ",errorText="Dette var ikke et heltall",forventet=[i for i in range(len(dir_list))])
     current_dir +="/"+dir_list[brukerInput]
+
+
+    while os.path.isdir(current_dir)==True:
+        print("")
+        print("-"*20)
+        dir_list = os.listdir(current_dir)
+        print("Her er en liste over filer:")
+        kB.print1DList(dir_list)
+        brukerInput = kB.betterInput(variableType="int",inputText=f"Hvilken fil vil du ha?(0-{len(dir_list)-1}): ",errorText="Dette var ikke et heltall",forventet=[i for i in range(len(dir_list))])
+        current_dir +="/"+dir_list[brukerInput]
+    
+    return current_dir
+
+
+if __name__ =="__main__":
+    print(finnFil(os.getcwd()))
